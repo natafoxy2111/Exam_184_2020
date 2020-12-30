9fd2398773e14d3ecff6924f82b3ff6d790c0f1b
Наталья Володина <70664421+natafoxy2111@users.noreply.github.com>
2020-12-30 10:11:41 +0300

    Add files via upload

---
+++ Volodina_exam184.cpp
@@ -0,0 +1,36 @@
+﻿// Volodina_exam184.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//Написать класс, содержащий метод, находящий два числа, произведение которых является максимальным в заданной последовательности чисел. Последовательность задается массивом и передаются в виде параметра метода. Показать пример использования реализованного класса.
+
+#include <iostream>
+#include <locale.h>
+using namespace std;
+
+class Maxmin
+{
+public:
+
+	int proizvedenie(int* a, int n)
+	{
+		int prod = INT_MIN;
+		for (int i = 0; i < n; i++)
+			for (int j = 0; j < n; j++)
+				if (a[i] * a[j] > prod && i != j)
+				{
+					prod = a[i] * a[j];
+				}
+		return prod;
+	}
+};
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n;
+	cout << "Введите количество чисел в последовательности: " << endl;
+	cin >> n;
+	int* a = new int[n];
+	cout << endl << "Введите последовательность из " << n << " чисел:" << endl;
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	Maxmin max;
+	cout << max.proizvedenie(a, n);
+}
